006786ac036c2fc4162d5eb931afe5317318a477
Юлия (Джулия) <sixquestions666@gmail.com>
2025-10-18 16:26:02 +0300

    Create pages/urls.py

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def data_view(request):
+    return HttpResponse("Страница DATA")
+def test_view(request):
+    return HttpResponse("Страница TEST")
 
 # Create your views here.
