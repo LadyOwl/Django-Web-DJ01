8944e4f80d7b34499a5f78b54fb071c06794a6ae
Юлия (Джулия) <sixquestions666@gmail.com>
2025-10-18 16:55:17 +0300

    Update

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
+    path('', views.home_view, name='home'),      # ← добавь эту строку: главная страница = data
     path('data/', views.data_view),
     path('test/', views.test_view),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+def home_view(request):
+    return HttpResponse("<p1>Главная страница</p1>")
 def data_view(request):
-    return HttpResponse("Страница DATA")
+    return HttpResponse("<p1>Страница DATA</p1>")
 def test_view(request):
-    return HttpResponse("Страница TEST")
+    return HttpResponse("<p1>Страница TEST</p1>")
 
 # Create your views here.
