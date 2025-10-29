d330b3287d7e9e0a7bdd9e6bb1d89b05bf39728f
Юлия (Джулия) <sixquestions666@gmail.com>
2025-10-29 19:47:18 +0300

    Update  urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('data/', views.data_view),
     path('test/', views.test_view),
+    path('page3/', views.page3_view, name='page3'),
+    path('page4/', views.page4_view, name='page4'),
 ]
